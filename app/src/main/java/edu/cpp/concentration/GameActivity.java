39ad8193ac9a4822693e346ffd11fe2deeba64e3
andrew-tek <andrewtek@gmail.com>
2017-11-28 19:39:14 -0800

    Added game activity

--- app/src/main/java/edu/cpp/concentration/GameActivity.java
+++ app/src/main/java/edu/cpp/concentration/GameActivity.java
@@ -1,6 +1,25 @@
 package edu.cpp.concentration;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class GameActivity extends AppCompatActivity {
 
 
-public class GameActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Log.w("hello", "onCreate: DAMNIT");
+
+    }
 }
--- app/src/main/java/edu/cpp/concentration/InfoActivity.java
+++ app/src/main/java/edu/cpp/concentration/InfoActivity.java
@@ -11,17 +11,26 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class InfoActivity extends AppCompatActivity {
-
     NumberPicker numberPicker;
-
+    @BindView(R.id.submitButton)
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+        numberPicker = findViewById(R.id.numberPicker);
         String [] values = {"4", "6", "8", "10", "12", "14", "16", "18", "20"};
         numberPicker.setMinValue(0);
         numberPicker.setMaxValue(values.length - 1);
         numberPicker.setDisplayedValues(values);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.submitButton)
+    public void moveToGameActivity() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
 }
