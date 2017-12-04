4d21949a19b4d362ab85b98355289102acf0ea3a
zerrisk <zerrisk@gmail.com>
2017-12-03 17:41:36 -0800

    reading from file seems to be working

--- app/src/main/java/edu/cpp/concentration/MainActivity.java
+++ app/src/main/java/edu/cpp/concentration/MainActivity.java
@@ -1,13 +1,20 @@
 package edu.cpp.concentration;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        highScoreFileToMemory();
         ButterKnife.bind(this);
     }
 
@@ -31,6 +39,52 @@ public class MainActivity extends AppCompatActivity {
     public void changeScreenToHighScoreInfo() {
         Intent intent = new Intent (this, HighScoreInfoActivity.class);
         startActivity(intent);
+    }
+
+    // method: highScoreFileToMemory
+    // purpose: takes the high scores file associated with the difficulty and places it in internal memory if it isn't there
+    private void highScoreFileToMemory() {
+        String filename = "-highscores.txt";
+        String appendedFilename;
+
+        try {
+            for (int i = 4; i <= 20; i+=2) {
+                // i = number, so #-highscores.txt (see assets folder)
+                appendedFilename = i + filename;
+
+                File file = getBaseContext().getFileStreamPath(appendedFilename);
+                if (!file.exists()) {
+                    FileOutputStream fos = openFileOutput(appendedFilename, Context.MODE_PRIVATE);
+                    fos.write(loadFile(appendedFilename).getBytes());
+                    fos.close();
+                }
+                else {
+                    // File exists
+                    Log.i("FILE_EXIST", appendedFilename + " EXISTS");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // method: loadFile
+    // purpose: retrieve contents of file
+    private String loadFile(String file) {
+        String contents = "";
+        try {
+            InputStreamReader isr = new InputStreamReader(getAssets().open(file));
+            BufferedReader br = new BufferedReader(isr);
+            // Read 3 lines (top 3 scores)
+            for (int i = 0; i < 3; i++) {
+                contents += br.readLine() + "\n";
+            }
+            isr.close();
+            br.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return contents;
     }
 
 }
---
+++ app/src/main/java/edu/cpp/concentration/Score.java
@@ -0,0 +1,48 @@
+package edu.cpp.concentration;
+
+
+
+public class Score {
+
+    // name of player or user
+    private String name;
+    // player's high score
+    private int score;
+
+    // method: Scores
+    // purpose: constructor for player name and score
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    // method: Scores
+    // purpose: constructor for player score
+    public Score(int score) {
+        this.score = score;
+    }
+
+    // method: getPlayer
+    // purpose: return name of player (String)
+    public String getName() {
+        return name;
+    }
+
+    // method: getScore
+    // purpose: return score of player
+    public int getScore() {
+        return score;
+    }
+
+    // method: setPlayer
+    // purpose: set name of player
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // method: setScore
+    // purpose: set score of player
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
